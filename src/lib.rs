d20581a622682bf6c019bdd42d1a8a46279646d9
rhysd <lin90162@yahoo.co.jp>
2019-01-12 21:22:30 +0900

    add document

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,34 @@
+//! A library for converting termcolor::Color to RGB
+//!
+//! ```rust
+//! use termcolor;
+//!
+//! // Declare extending termcolor::Color by using a trait.
+//! // .to_rgb() method is made available.
+//! use termcolor2rgb::ColorExt;
+//!
+//! // Basic 8 colors
+//! assert_eq!(termcolor::Color::Red.to_rgb(), (0x80, 0, 0));
+//!
+//! // ANSI 256 colors
+//! assert_eq!(termcolor::Color::Ansi256(223).to_rgb(), (0xff, 0xd7, 0xaf));
+//!
+//! // RGB color
+//! assert_eq!(termcolor::Color::Rgb(0x12, 0x89, 0xef).to_rgb(), (0x12, 0x89, 0xef));
+//! ```
+//!
+//! termcolor: https://crates.io/crates/termcolor
 extern crate termcolor;
 
 use termcolor::Color;
 
+/// Trait to extend termcolor::Color.
 pub trait ColorExt {
     fn to_rgb(&self) -> (u8, u8, u8);
 }
 
 impl ColorExt for Color {
+    /// Convert a `Color` instance to RGB color. Returned tuple means (R, G, B) in `u8` values.
     fn to_rgb(&self) -> (u8, u8, u8) {
         use termcolor::Color::*;
         match self {
